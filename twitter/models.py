d4c9b34fe472ecae45f311a0a0795ba97ea64a76
Julio-PC\Julio <julio.llinas@gmail.com>
2025-01-10 18:13:45 -0400

    create settings,login,style,layaout and update migations

--- twitter/models.py
+++ twitter/models.py
@@ -5,21 +5,22 @@ from django.utils import timezone
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.CharField(default='Hola, Twitter', max_length=100)
-    #image
-    
+    bio = models.CharField(default='Hola, twitter', max_length=100)
+    image = models.ImageField(default='default.png')
+
     def __str__(self):
-        return f'Perfil de {self.user.username}'
-    
+        return f"Perfil de {self.user.username}"  # Corregida la sangría
+
 class Post(models.Model):
     timestamp = models.DateTimeField(default=timezone.now)
-    content = models.TimeField()
+    content = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
-    
+    action_checkbox = models.BooleanField(default=False)
+
     class Meta:
         ordering = ['-timestamp']
-        
+
     def __str__(self):
-        return self.content
+        return self.content  
         
     
