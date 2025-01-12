06aa339b07a19bbc19b4a8a2a22a50fdfd2c7726
Julio-PC\Julio <julio.llinas@gmail.com>
2025-01-11 21:49:51 -0400

    image user

--- django_project/settings.py
+++ django_project/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,3 +135,5 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- twitter/urls.py
+++ twitter/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
 	path('register/', views.register, name='register'),
 	path('login/', LoginView.as_view(template_name='login.html'), name='login'),
 	path('logout/', LogoutView.as_view(), name='logout'),
-]
+	path('delete/<int:post_id>/', views.delete, name='delete'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
